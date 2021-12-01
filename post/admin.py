c07799602c846616c28d1413ddf0c31ad6fde43e
zankoatzanko <zankoan4@gmail.com>
2021-12-01 03:39:02 +0330

    Remove an extra import and display thumbnail of the posts

--- post/admin.py
+++ post/admin.py
@@ -1,16 +1,16 @@
 from django.contrib import admin
 from post.models import Post, Comment
-from django.utils.translation import gettext_lazy as _
 
 
 @admin.register(Post)
-class ArticleAdmin(admin.ModelAdmin):
-    list_display = ("author", "title", "publish_date", "status")
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("author", "thumbnail_preview", "title", "publish_date", "status")
     prepopulated_fields = {"slug": ("title",)}
     list_filter = ("author", "status")
     list_editable = ("status",)
     list_display_links = ("title",)
-    
+
+
 @admin.register(Comment)
 class Data(admin.ModelAdmin):
     pass
