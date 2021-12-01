049a7a9079dd557102d8e91a9d494ebddc1f6c09
zankoatzanko <zankoan4@gmail.com>
2021-12-01 03:34:08 +0330

    Add tinymce urls

--- core/urls.py
+++ core/urls.py
@@ -3,9 +3,11 @@ from django.urls import path, include
 from django.conf.urls.static import static 
 from django.conf import settings
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("post.urls", namespace="Posts")),
-    path("account/", include("accounts.urls", namespace="Account"))
+    path("account/", include("accounts.urls", namespace="Account")),
+    path('tinymce/', include('tinymce.urls')),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  
