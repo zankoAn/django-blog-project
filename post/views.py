b94840cc107e700a7a6881a0f4db76ae424ac4cf
zankoatzanko <zankoan4@gmail.com>
2021-12-01 13:06:53 +0330

    Update post detail view

--- post/views.py
+++ post/views.py
@@ -7,7 +7,6 @@ from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import login_required
 
 from .models import Post
-
 from datetime import datetime
 
 
@@ -26,6 +25,7 @@ class HomePageView(ListView):
         """
             Show the last 6 edited posts.
         """
+
         posts = Post.objects.all().order_by("-update_date", "-id")[:6]
         return posts
 
@@ -37,6 +37,10 @@ class LikeView(UpdateView):
     template_name = "post/home_page.html"
 
     def get(self, request, *args, **kwargs):
+        """
+            Get request and decrease or increase the number of likes.
+        """
+
         pk = kwargs.get("pk")
         user = request.user
         post = get_object_or_404(Post, pk=pk)
@@ -44,21 +48,39 @@ class LikeView(UpdateView):
         if user in post.likes.all():
             post.likes.remove(user)
             like_count = post.likes.count()
-            return JsonResponse({"status": "ok", "like_count": like_count, "class": "bi bi-heart"})
-
+            
+            return JsonResponse({
+                "status": "ok",
+                "like_count": like_count,
+                "class": "bi bi-heart"})
         else:
             post.likes.add(user)
             like_count = post.likes.count()
-            return JsonResponse({"status": "ok", "like_count": like_count, "class": "bi bi-heart-fill"})
-   
+
+            return JsonResponse({
+                "status": "ok",
+                "like_count": like_count,
+                "class": "bi bi-heart-fill"})
+  
+
 class PostDetailView(DetailView):
-    template_name = "post/post.html"
+    template_name = "post/post_detail.html"
     context_object_name = "post"
 
+    # The following line is just a test and will be deleted later...
+    extra_context = {"testa":["https://mdbootstrap.com/img/Photos/Avatars/img%20(33).jpg", ], "inner_comment":False, "comments_inner":[2]}
+
     def get_queryset(self):
-        date = datetime(self.kwargs["year"], self.kwargs["month"], self.kwargs["day"]).date()
+        date = datetime(
+            self.kwargs["year"],
+            self.kwargs["month"],
+            self.kwargs["day"]).date()
+        
         post_id = self.kwargs["pk"]
         post = Post.objects.filter(pk=post_id, publish_date=date)
-        print(post)
         return post
     
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["user_can_like"] = Post.user_can_like(Post, self.request.user)
+        return context
